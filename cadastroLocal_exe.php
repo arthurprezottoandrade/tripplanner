9306556b3d1a2ffc5fecba0fc6f5aedb0ab9c8e4
Matheus Henrique Rosa <71407848+mhrrosa@users.noreply.github.com>
2023-06-19 10:59:42 -0300

    Update cadastroLocal_exe.php

--- cadastroLocal_exe.php
+++ cadastroLocal_exe.php
@@ -1,62 +1,63 @@
 <html>
-    <head>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="CSS/cadastro.css">
     <title>Cadastro de Usuario - Tripp Planner</title>
     <link rel="icon" type="image/jpg" href="IMG/logo_icone.jpg"/>
-
-    </head>
-	<header class="cabecalho">
-    <style>
-                /* Estilos para o botão e o menu */
-                .menu-btn {
-                    position: fixed;
-                    top: 2%;
-                    right: 0;
-                    transform: translate(-50%, -50%);
-                    background-color: #f1f1f1;
-                    cursor: pointer;
-                }
-                
-                .menu {
-                    position: fixed;
-                    top: 50%;
-                    right: -15%; /* Inicialmente, o menu estará oculto */
-                    width: 300px;
-                    padding: 10px;
-                    border-radius: 185%;
-                    transition: right 0.7s;
-                }
-                
-                .menu a {
-                    display: block;
-                    margin-bottom: 5%;
-                }
-
-            </style>
-
-            <div class="menu-btn" onclick="toggleMenu()">Menu</div>
-            <div class="menu" id="menu">
-                <a href="inicio.php">Inicio</a>
-                <a href="cadastroUsuario.php">Cadastrar Usuário</a>
-                <a href="mostrarLocal.php">Locais Cadastrados</a>
-                <a href="cadastroLocal.php">Cadastrar Local</a>
-            </div>
+</head>
+<body>
+    <header class="cabecalho">
+        <style>
+            /* Estilos para o botão e o menu */
+            .menu-btn {
+                position: fixed;
+                top: 2%;
+                right: 0;
+                transform: translate(-50%, -50%);
+                background-color: #f1f1f1;
+                cursor: pointer;
+            }
             
-            <script>
-                function toggleMenu() {
-                    var menu = document.getElementById("menu");
-                    
-                    if (menu.style.right === "-13%") {
-                        menu.style.right = "0";
-                    } else {
-                        menu.style.right = "-13%";
-                    }
+            .menu {
+                position: fixed;
+                top: 50%;
+                right: -15%; /* Inicialmente, o menu estará oculto */
+                width: 300px;
+                padding: 10px;
+                border-radius: 185%;
+                transition: right 0.7s;
+            }
+            
+            .menu a {
+                display: block;
+                margin-bottom: 5%;
+            }
+        </style>
+
+        <div class="menu-btn" onclick="toggleMenu()">Menu</div>
+        <div class="menu" id="menu">
+            <a href="inicio.php">Inicio</a>
+            <a href="cadastroUsuario.php">Cadastrar Usuário</a>
+            <a href="mostrarLocal.php">Locais Cadastrados</a>
+            <a href="cadastroLocal.php">Cadastrar Local</a>
+        </div>
+        
+        <script>
+            function toggleMenu() {
+                var menu = document.getElementById("menu");
+                
+                if (menu.style.right === "-13%") {
+                    menu.style.right = "0";
+                } else {
+                    menu.style.right = "-13%";
                 }
-            </script>
+            }
+        </script>
+
         <div>
             <img class="logo" src="IMG/logopng.png"/>
         </div>
+
         <div class="botao-cabecalho">
             <ul>
                 <li>
@@ -77,71 +78,70 @@
             </ul>
         </div>
     </header>
-	<header class="linha-divisao"></header>
 
-        <!-- Retângulo Principal: deslocado em 270 pixels para direita quando é visível -->
-        <div class="w3-main w3-container">
+    <header class="linha-divisao"></header>
 
-            <!-- Borda do Retângulo Principal -->
-            <div class="w3-panel w3-padding-large w3-card-4 w3-light-brown">
-    
-                <h1>Registro de Locais</h1>
-                <?php require 'conectaBD.php'; ?>
+    <!-- Retângulo Principal: deslocado em 270 pixels para direita quando é visível -->
+    <div class="w3-main w3-container">
 
-                <?php
-                    $nome    = $_POST['nome'];
-                    $rua = $_POST['rua'];
-                    $numero = $_POST['numero'];
-                    $bairro = $_POST['bairro'];
-                    $cidade = $_POST['cidade'];
-                    $preferencia = $_POST['preferencia'];
-                    $descricao = $_POST['descricao'];
+        <!-- Borda do Retângulo Principal -->
+        <div class="w3-panel w3-padding-large w3-card-4 w3-light-brown">
+            <h1>Registro de Locais</h1>
+            <?php require 'conectaBD.php'; ?>
 
-                    $name = $_FILES['Imagem']['name'];
-                    $target_dir = "IMG/";
-                    $target_file = $target_dir . basename($_FILES["Imagem"]["name"]);
-                    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                    $extensions_arr = array("jpg","jpeg","png","gif");
+            <?php
+                $nome = $_POST['nome'];
+                $rua = $_POST['rua'];
+                $numero = $_POST['numero'];
+                $bairro = $_POST['bairro'];
+                $cidade = $_POST['cidade'];
+                $preferencia = $_POST['preferencia'];
+                $descricao = $_POST['descricao'];
 
-                    if( in_array($imageFileType,$extensions_arr) ){
+                $name = $_FILES['Imagem']['name'];
+                $target_dir = "IMG/";
+                $target_file = $target_dir . basename($_FILES["Imagem"]["name"]);
+                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                $extensions_arr = array("jpg", "jpeg", "png", "gif");
 
+                if (in_array($imageFileType, $extensions_arr)) {
                     // Upload do arquivo
-                    if(move_uploaded_file($_FILES['foto']['tmp_name'],$target_dir.$name)){
-                        // Convertendo para base 64
-                        $image_base64 = base64_encode(file_get_contents('IMG/'.$name) );
+                    if (move_uploaded_file($_FILES['Imagem']['tmp_name'], $target_dir . $name)) {
+                        // Convertendo para base64
+                        $image_base64 = base64_encode(file_get_contents('IMG/' . $name));
                         // Inserindo 
-                        $sql = "INSERT INTO locais(nome, rua, numero, bairro, cidade, foto,tipopref,descricao) VALUES ('$nome','$rua', '$numero','$bairro','$cidade', '$image_base64','$preferencia','$descricao')";
-                        } 
-                    } else {
-                        $sql = "INSERT INTO locais(nome, rua, numero, bairro, cidade, tipopref, descricao) VALUES ('$nome','$rua', '$numero','$bairro','$cidade','$preferencia','$descricao')";
+                        $sql = "INSERT INTO locais (nome, rua, numero, bairro, cidade, foto, tipopref, descricao) VALUES ('$nome', '$rua', '$numero', '$bairro', '$cidade', '$image_base64', '$preferencia', '$descricao')";
                     }
+                } else {
+                    $sql = "INSERT INTO locais (nome, rua, numero, bairro, cidade, tipopref, descricao) VALUES ('$nome', '$rua', '$numero', '$bairro', '$cidade', '$preferencia', '$descricao')";
+                }
 
-                    // Cria conexão
-                    $conn = mysqli_connect($servername, $username, $password, $database);
+                // Cria conexão
+                $conn = mysqli_connect($servername, $username, $password, $database);
 
-                    // Verifica conexão 
-                    if (!$conn) {
-                        die("Falha na conexão com o Banco de Dados: " . mysqli_connect_error());
-                    }
+                // Verifica conexão 
+                if (!$conn) {
+                    die("Falha na conexão com o Banco de Dados: " . mysqli_connect_error());
+                }
 
-                    // Configura para trabalhar com caracteres acentuados do português
-                    mysqli_query($conn, "SET NAMES 'utf8'");
-                    mysqli_query($conn, 'SET character_set_connection=utf8');
-                    mysqli_query($conn, 'SET character_set_client=utf8');
-                    mysqli_query($conn, 'SET character_set_results=utf8');
+                // Configura para trabalhar com caracteres acentuados do português
+                mysqli_query($conn, "SET NAMES 'utf8'");
+                mysqli_query($conn, 'SET character_set_connection=utf8');
+                mysqli_query($conn, 'SET character_set_client=utf8');
+                mysqli_query($conn, 'SET character_set_results=utf8');
 
-                    // Faz Select na Base de Dados
-                    echo "<div class='w3-responsive w3-card-4'>";
-                    if ($result = mysqli_query($conn, $sql)) {
-                        echo "Um registro adicionado!";
-                    } else {
-                        echo "Erro executando INSERT: " . mysqli_error($conn);
-                    }
-                    echo "</div>";
-                    mysqli_close($conn);
-                ?>
-     
-     </div>
+                // Executa o INSERT na base de dados
+                echo "<div class='w3-responsive w3-card-4'>";
+                if (mysqli_query($conn, $sql)) {
+                    echo "Um registro adicionado!";
+                } else {
+                    echo "Erro executando INSERT: " . mysqli_error($conn);
+                }
+                echo "</div>";
+
+                mysqli_close($conn);
+            ?>
         </div>
-	</body>
+    </div>
+</body>
 </html>
