199e1c44369e8f14a75c168262604fb93ad31024
Matheus Henrique Rosa <71407848+mhrrosa@users.noreply.github.com>
2023-06-19 11:45:30 -0300

    Update cadastroUsuario.php

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -1,132 +1,130 @@
+<!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="CSS/cadastro.css">
-    <title>Cadastro de Usuario - Tripp Planner</title>
-    <link rel="icon" type="image/jpg" href="IMG/logo_icone.jpg"/>
-</head>
-<body>
-    <header class="cabecalho">
+    <head>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="CSS/cadastro.css">
+        <title>Cadastro de Usuario - Tripp Planner</title>
+        <link rel="icon" type="image/jpg" href="IMG/logo_icone.jpg"/>
+    </head>
+    <body>
+    <!-- CONEXÃO COM O BANCO DE DADOS -->
+    <?php require 'conectaBD.php'; ?>
+        <!-- CABEÇALHO -->
+        <header class="cabecalho">
         <style>
-            /* Estilos para o botão e o menu */
-            .menu-btn {
-                position: fixed;
-                top: 2%;
-                right: 0;
-                transform: translate(-50%, -50%);
-                background-color: #f1f1f1;
-                cursor: pointer;
-            }
+                /* Estilos para o botão e o menu */
+                .menu-btn {
+                    position: fixed;
+                    top: 2%;
+                    right: 0;
+                    transform: translate(-50%, -50%);
+                    background-color: #f1f1f1;
+                    cursor: pointer;
+                }
+                
+                .menu {
+                    position: fixed;
+                    top: 50%;
+                    right: -15%; /* Inicialmente, o menu estará oculto */
+                    width: 300px;
+                    padding: 10px;
+                    border-radius: 185%;
+                    transition: right 0.7s;
+                }
+                
+                .menu a {
+                    display: block;
+                    margin-bottom: 5%;
+                }
+
+            </style>
+
+            <div class="menu-btn" onclick="toggleMenu()">Menu</div>
+            <div class="menu" id="menu">
+                <a href="inicio.php">Inicio</a>
+                <a href="cadastroUsuario.php">Cadastrar Usuário</a>
+                <a href="mostrarLocal.php">Locais Cadastrados</a>
+                <a href="cadastroLocal.php">Cadastrar Local</a>
+            </div>
             
-            .menu {
-                position: fixed;
-                top: 50%;
-                right: -15%; /* Inicialmente, o menu estará oculto */
-                width: 300px;
-                padding: 10px;
-                border-radius: 185%;
-                transition: right 0.7s;
-            }
-            
-            .menu a {
-                display: block;
-                margin-bottom: 5%;
-            }
-        </style>
-
-        <div class="menu-btn" onclick="toggleMenu()">Menu</div>
-        <div class="menu" id="menu">
-            <a href="inicio.php">Inicio</a>
-            <a href="cadastroUsuario.php">Cadastrar Usuário</a>
-            <a href="mostrarLocal.php">Locais Cadastrados</a>
-            <a href="cadastroLocal.php">Cadastrar Local</a>
-        </div>
-        
-        <script>
-            function toggleMenu() {
-                var menu = document.getElementById("menu");
-                
-                if (menu.style.right === "-13%") {
-                    menu.style.right = "0";
-                } else {
-                    menu.style.right = "-13%";
+            <script>
+                function toggleMenu() {
+                    var menu = document.getElementById("menu");
+                    
+                    if (menu.style.right === "-13%") {
+                        menu.style.right = "0";
+                    } else {
+                        menu.style.right = "-13%";
+                    }
                 }
-            }
-        </script>
+            </script>
+            <main>
+                <div>
+                    <div>
+                        <img class="logo" src="IMG/logopng.png"/>
+                    </div>
+                    <div class="botao-cabecalho">
+                        <ul>
+                            <li>
+                                <a href="">
+                                    <h3>MENU</h3>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>SOBRE</h3>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>CONTATO</h3>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </main>
+        </header>
+        <!-- LINHA DE DIVISÃO -->
+        <header class="linha-divisao"></header>
+        <!-- FORMULÁRIO -->
+        <header>
+                <div class="formcadastro">
+                    <form id="cadastro" action="cadastroUsuario_exe.php" method="post" onsubmit="return check(this.form)">
+                        <div class="form">
+                            <label class="titulo-form" for="text"><b>CADASTRO DE USUARIO</b></label>
+                            <label for="name"> Nome 
+                                <input type="text" name="nome" required>
+                            </label>
+                            <label for="sobrenome"> Sobrenome 
+                                <input type="text" name="sobrenome" required>
+                            </label>
 
-        <div>
-            <img class="logo" src="IMG/logopng.png"/>
-        </div>
+                            <label for="email"> E-mail
+                                <input type="email" name="email"required>
+                            </label>
+                            <label for="senha"> Senha 
+                                <input type="password" name="senha"required>
+                            </label>
 
-        <div class="botao-cabecalho">
-            <ul>
-                <li>
-                    <a href="inicio.php">
-                        <h3>MENU</h3>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <h3>SOBRE</h3>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <h3>CONTATO</h3>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </header>
-
-    <header class="linha-divisao"></header>
-
-    <!-- Retângulo Principal: deslocado em 270 pixels para direita quando é visível -->
-    <div class="w3-main w3-container">
-
-        <!-- Borda do Retângulo Principal -->
-        <div class="w3-panel w3-padding-large w3-card-4 w3-light-brown">
-            <h1>Registro de Usuario</h1>
-            <?php require 'conectaBD.php'; ?>
-
-            <?php
-                $nome = $_POST['nome'];
-                $sobrenome = $_POST['sobrenome'];
-                $email = $_POST['email'];
-                $senha = $_POST['senha'];
-                
-
-
-       
-                $sql = "INSERT INTO usuario (nome, sobrenome, email, senha) VALUES ('$nome', '$sobrenome', '$email', '$senha')";
-                
-
-                // Cria conexão
-                $conn = mysqli_connect($servername, $username, $password, $database);
-
-                // Verifica conexão 
-                if (!$conn) {
-                    die("Falha na conexão com o Banco de Dados: " . mysqli_connect_error());
-                }
-
-                // Configura para trabalhar com caracteres acentuados do português
-                mysqli_query($conn, "SET NAMES 'utf8'");
-                mysqli_query($conn, 'SET character_set_connection=utf8');
-                mysqli_query($conn, 'SET character_set_client=utf8');
-                mysqli_query($conn, 'SET character_set_results=utf8');
-
-                // Executa o INSERT na base de dados
-                echo "<div class='w3-responsive w3-card-4'>";
-                if (mysqli_query($conn, $sql)) {
-                    echo "Um registro adicionado!";
-                } else {
-                    echo "Erro executando INSERT: " . mysqli_error($conn);
-                }
-                echo "</div>";
-
-                mysqli_close($conn);
-            ?>
-        </div>
-    </div>
-</body>
+                            <label for="submit"> 
+                                <button class="botao-cadastro" type="submit"><b>Cadastrar</b></button>
+                            </label>
+                        </div>
+                    </form>
+                </div>
+            </header>
+        <!-- RODAPÉ -->
+        <footer>
+            <header class="linha-divisao"></header>
+            <img class="img-rodape" src="IMG/logo_verticalpng.png">
+            <p class="copyright">&copy; Copyright Tripp Planner - 2023</p>
+        </footer>
+        <?php 
+            //FIM DA DIV FORM
+            echo "</div>";
+            //ENCERRA CONEXÃO COM O BANCO DE DADOS
+            mysqli_close($conn);
+        ?>
+    </body>
 </html>
