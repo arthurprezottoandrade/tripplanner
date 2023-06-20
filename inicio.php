9d7c7f77b6b1ccb609cfb08090add77ccab5592c
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2023-06-20 10:55:37 -0300

    Atualização

--- inicio.php
+++ inicio.php
@@ -40,7 +40,7 @@
         <div class="w3-main w3-container">
 
             <div class="w3-panel w3-padding-large w3-card-4 w3-light-dark">
-                <h2 class="w3-xxlarge"style="text-align: center">Seu novo melhor amigo está aqui</h2>
+                <h2 class="w3-xxlarge"style="text-align: center">Sua nova viagem está aqui</h2>
 
                 <p class="w3-large">
                 <div class="w3-code">  
@@ -87,7 +87,7 @@
                                 <img class="fotoConvertida" src="data:image/png;base64,<?php echo $foto ?>">
                             </div>
                             <a href='reservaCachorro.php?id=<?php echo $id_cachorro ?>'>
-                                <button class="botao-reserva">Reservar</button>
+                                <button class="botao-reserva">Favoritar</button>
                             </a>
                         </div>
                     </div>
@@ -106,7 +106,7 @@
         <footer>
             <header class="linha-divisao"></header>
             <img class="img-rodape" src="IMG/logo_verticalpng.png">
-            <p class="copyright">&copy; Copyright Help Friend - 2022</p>
+            <p class="copyright">&copy; Copyright Trip Planer - 2023</p>
         </footer>
     </body>
 </html>
