40551cbb1a92fafe95681354ee00f13579bc80b9
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2023-06-21 14:57:09 -0300

    Ajuste de filtro

--- inicio.php
+++ inicio.php
@@ -103,121 +103,175 @@
             .modal {
                 display: none;
                 position: fixed;
+                visibility: hidden;
                 z-index: 1;
                 left: 0;
                 top: 0;
                 width: 100%;
                 height: 100%;
                 overflow: auto;
-                background-color: rgba(0,0,0,0.4);
+                background-color: rgba(0, 0, 0, 0.4);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .modal.show {
+                display: flex;
+                visibility: visible;
+            }
+
+            .modal-header {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
             }
 
             .modal-content {
                 background-color: #fefefe;
-                margin: 15% auto;
                 padding: 20px;
                 border: 1px solid #888;
                 width: 80%;
+                max-height: 80vh;
+                overflow-y: auto;
+                display: grid;
+                grid-template-columns: auto 1px auto;
+                align-items: center;
+                justify-content: center;
+                grid-column-gap: 20px; /* Adiciona um espaçamento de 20px entre as colunas */
             }
 
+            .cidade-filters {
+                grid-column: 1 / 2;
+            }
+
+            .separator {
+                grid-column: 2 / 3;
+                margin: 0 10px;
+                border-left: 1px solid #ccc;
+                height: 100%;
+            }
+
+            .local-filters {
+                grid-column: 3 / 4;
+            }
+
+            .form-grid {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                gap: 20px; /* Adiciona um espaçamento de 20px entre as divs */
+            }
+                        
             .close {
                 color: #aaa;
-                float: right;
                 font-size: 28px;
                 font-weight: bold;
                 cursor: pointer;
             }
 
             .center {
-                text-align: center;
-                position: relative;
+            text-align: center;
+            position: relative;
+            }
+            .checkbox-label {
+                display: flex;
+                align-items: center;
             }
 
             .brown-button {
-                background-color: #5C4033;
-                color: #fff;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 4px;
-                font-size: 16px;
-                cursor: pointer;
-                transform: translateX(-120%);
+            background-color: #5c4033;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+            transform: translateX(-120%);
             }
 
             .form-grid {
-                display: grid;
-                grid-template-columns: repeat(2, 1fr);
-                gap: 10px;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 100px;
             }
 
             .submit-button {
-                display: flex;
-                justify-content: center;
+            display: flex;
+            justify-content: center;
             }
 
             .submit-button input[type="submit"] {
-                background-color: #5C4033;
-                color: #fff;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 4px;
-                font-size: 16px;
-                cursor: pointer;
+            background-color: #5c4033;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
             }
 
             </style>
 
             <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <span class="close" onclick="closeModal()">&times;</span>
-                    <form method="POST">
-                        <h2>Selecione as opções:</h2>
-                        <div class="form-grid">
-                            <label for="opcao1">
-                                <input type="checkbox" name="opcao1" value="1"> Opção 1
-                            </label>
-                            <label for="opcao2">
-                                <input type="checkbox" name="opcao2" value="2"> Opção 2
-                            </label>
-                            <label for="opcao3">
-                                <input type="checkbox" name="opcao3" value="3"> Opção 3
-                            </label>
-                            <label for="opcao4">
-                                <input type="checkbox" name="opcao4" value="4"> Opção 4
-                            </label>
-                            <label for="opcao5">
-                                <input type="checkbox" name="opcao5" value="5"> Opção 5
-                            </label>
-                            <label for="opcao6">
-                                <input type="checkbox" name="opcao6" value="6"> Opção 6
-                            </label>
-                            <label for="opcao7">
-                                <input type="checkbox" name="opcao7" value="7"> Opção 7
-                            </label>
-                            <label for="opcao8">
-                                <input type="checkbox" name="opcao8" value="8"> Opção 8
-                            </label>
-                            <label for="opcao9">
-                                <input type="checkbox" name="opcao9" value="9"> Opção 9
-                            </label>
-                            <label for="opcao10">
-                                <input type="checkbox" name="opcao10" value="10"> Opção 10
-                            </label>
-                        </div>
-                        <br><br>
-                        <div class="submit-button">
-                            <input type="submit" value="Enviar">
-                        </div>
-                    </form>
+            <div class="modal-content">
+            <form method="POST">
+                <div class="modal-header">
+                    <h2>Selecione as opções:</h2>
+                    <span class="close" onclick="closeModal()">&times;</span> 
                 </div>
+                <div class="form-grid">
+                    <div class="cidade-filters">
+                    <h3>Cidades</h3>
+                    <label for="cidade1" class="checkbox-label">
+                        <input type="checkbox" name="cidade1" value="a"> Opção 1
+                    </label>
+                    <label for="cidade2" class="checkbox-label">
+                        <input type="checkbox" name="cidade2" value="b"> Opção 2
+                    </label>
+                    <label for="cidade3" class="checkbox-label">
+                        <input type="checkbox" name="cidade3" value="c"> Opção 3
+                    </label>
+                    <label for="cidade4" class="checkbox-label">
+                        <input type="checkbox" name="cidade4" value="d"> Opção 4
+                    </label>
+                    <label for="cidade5" class="checkbox-label">
+                        <input type="checkbox" name="cidade5" value="e"> Opção 5
+                    </label>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="local-filters">
+                    <h3>Locais</h3>
+                    <label for="local1" class="checkbox-label">
+                        <input type="checkbox" name="local1" value="1"> Opção 6
+                    </label>
+                    <label for="local2" class="checkbox-label">
+                        <input type="checkbox" name="local2" value="2"> Opção 7
+                    </label>
+                    <label for="local3" class="checkbox-label">
+                        <input type="checkbox" name="local3" value="3"> Opção 8
+                    </label>
+                    <label for="local4" class="checkbox-label">
+                        <input type="checkbox" name="local4" value="4"> Opção 9
+                    </label>
+                    <label for="local5" class="checkbox-label">
+                        <input type="checkbox" name="local5" value="5"> Opção 10
+                    </label>
+                    </div>
+                </div>
+                <br><br>
+                <div class="submit-button">
+                    <input type="submit" value="Enviar">
+                </div>
+                </form>
+            </div>
             </div>
             <script>
                 function openModal() {
-                    document.getElementById("myModal").style.display = "block";
+                    document.getElementById("myModal").classList.add("show");
                 }
 
                 function closeModal() {
-                    document.getElementById("myModal").style.display = "none";
+                    document.getElementById("myModal").classList.remove("show");
                 }
             </script>
         </body>
@@ -244,48 +298,73 @@
 
                 // Faz Select na Base de Dados
 
-                $opcao1 = '1';
-                $opcao2 = '2';
-                $opcao3 = '3';
-                $opcao4 = '4';
-                $opcao5 = '5';
-                $opcao6 = '6';
-                $opcao7 = '7';
-                $opcao8 = '8';
-                $opcao9 = '9';
-                $opcao10 = '10';
+                $cidade1 = 'a';
+                $cidade2 = 'b';
+                $cidade3 = 'c';
+                $cidade4 = 'd';
+                $cidade5 = 'e';
+
+                $local1 = '1';
+                $local2 = '2';
+                $local3 = '3';
+                $local4 = '4';
+                $local5 = '5';
+
 
                 $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l";
 
-                $opcoesSelecionadas = [];
+                $cidadeSelecionada = [];
+                $localSelecionado = [];
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     // Armazenar os números selecionados
-                    $opcao1 = $_POST['opcao1'] ?? '';
-                    $opcao2 = $_POST['opcao2'] ?? '';
-                    $opcao3 = $_POST['opcao3'] ?? '';
-                    $opcao4 = $_POST['opcao4'] ?? '';
-                    $opcao5 = $_POST['opcao5'] ?? '';
-                    $opcao6 = $_POST['opcao6'] ?? '';
-                    $opcao7 = $_POST['opcao7'] ?? '';
-                    $opcao8 = $_POST['opcao8'] ?? '';
-                    $opcao9 = $_POST['opcao9'] ?? '';
-                    $opcao10 = $_POST['opcao10'] ?? '';
+                    $cidade1 = $_POST['cidade1'] ?? '';
+                    $cidade2 = $_POST['cidade2'] ?? '';
+                    $cidade3 = $_POST['cidade3'] ?? '';
+                    $cidade4 = $_POST['cidade4'] ?? '';
+                    $cidade5 = $_POST['cidade5'] ?? '';
+                    // ... outros filtros de cidade
+
+                    // Filtros de local
+                    $local1 = $_POST['local1'] ?? '';
+                    $local2 = $_POST['local2'] ?? '';
+                    $local3 = $_POST['local3'] ?? '';
+                    $local4 = $_POST['local4'] ?? '';
+                    $local5 = $_POST['local5'] ?? '';
     
                     // Fazer o que desejar com as variáveis $opcao1, $opcao2, $opcao3
                     // Neste exemplo, vamos apenas armazená-las em uma variável
-                    $opcoesSelecionadas = array($opcao1, $opcao2, $opcao3, $opcao4, $opcao5, $opcao6, $opcao7, $opcao8, $opcao9, $opcao10);
+                    $cidadeSelecionada = array($cidade1, $cidade2, $cidade3, $cidade4, $cidade5);
+                    $localSelecionado = array($local1, $local2, $local3, $local4, $local5);
                        // Verificar se o array está vazio
-                    $opcoesSelecionadas = array_filter($opcoesSelecionadas);
+                    $cidadeSelecionada = array_filter($cidadeSelecionada);
+                    $localSelecionado = array_filter($localSelecionado);
 
                     // Verificar se o array está vazio
-                    if (empty($opcoesSelecionadas)) {
-                        $busca = ''; // Valor padrão quando todos os valores estão vazios
+                    if (empty($cidadeSelecionada)) {
+                        $buscaCidade = ''; // Valor padrão quando todos os valores estão vazios
                     } else {
-                        $busca = implode(', ', array_map('intval', $opcoesSelecionadas));
+                        $cidadeSelecionadaQuoted = array_map(function($cidade) {
+                            return "'" . $cidade . "'";
+                        }, $cidadeSelecionada);
+                        $buscaCidade = implode(', ', $cidadeSelecionadaQuoted);
                     }
 
-                    $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l where tipopref in ($busca)";
-                    
+                    if (empty($localSelecionado)) {
+                        $buscaLocal = ''; // Valor padrão quando todos os valores estão vazios
+                    } else {
+                        $buscaLocal = implode(', ', array_map('intval', $localSelecionado));
+                    }
+
+
+                    if(empty($buscaCidade) && empty($buscaLocal)) {
+                        $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l";
+                    } else if(empty($buscaCidade)) {
+                        $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l where tipopref in ($buscaLocal)";
+                    } else if(empty($buscaLocal)) {
+                        $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l where cidade in ($buscaCidade)";
+                    } else {
+                        $sql = "SELECT l.id as Id, l.Nome as Nome, l.rua as Rua, l.numero as Numero, l.bairro as Bairro, l.cidade as Cidade, l.estado as Estado, l.foto as Imagem FROM locais l where cidade in ($buscaCidade) and tipopref in ($buscaLocal)";
+                    }
                 }
                 
                 $result = mysqli_query($conn, $sql);
